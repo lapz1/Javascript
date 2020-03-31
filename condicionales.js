7732625fc6c24be5a6cc847971f273d26d7578e6
Luis <lapz01@gmail.com>
2020-03-31 18:49:48 -0500

    Carga del archivo condicional

---
+++ condicionales.js
@@ -0,0 +1,8 @@
+var precio = 200000;
+var cantidad = 5;
+var totalVenta = precio * cantidad;
+var descuento = totalVenta > 877803 ? totalVenta * 0.2 : 0;
+var valorFinalVenta = totalVenta - descuento;
+console.log("Total antes de decuento: " + totalVenta);
+console.log("Valor del descuento: " + descuento);
+console.log("Total a pagar: " + valorFinalVenta);
