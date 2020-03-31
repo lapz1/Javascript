dde303e55621f03d510bd4240bea0f371d554d66
Luis <lapz01@gmail.com>
2020-03-31 18:16:37 -0500

    Creación del Repositorio

---
+++ server.js
@@ -0,0 +1,7 @@
+var precio = 1000;
+var cantidad = 3;
+var venta= precio * cantidad;
+var descuento = venta * 0.2;
+var total = venta - descuento;
+
+console.log ("El precio de la venta es " + venta + ", El decuento es de: " + descuento + ", El total es: " + total);
